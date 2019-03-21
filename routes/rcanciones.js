8025ded00b7186304385aea791a7acf353b61bc4
Daniel Fernández <daniferna@outlook.com>
2019-03-21 10:24:53 +0100

    “SDI-NodeJS- 8,9,10 Recursos estáticos, Peticiones POST y parámetros, Enrutamiento y comodines”

--- routes/rcanciones.js
+++ routes/rcanciones.js
@@ -1,5 +1,27 @@
-module.exports = function(app) {
-    app.get("/canciones", function(req, res) {
+module.exports = function (app) {
+
+    app.post("/cancion", function (req, res) {
+        res.send("Canción agregada:" + req.body.nombre + "<br>"
+            + " Género :" + req.body.genero + "<br>"
+            + " Precio: " + req.body.precio);
+    });
+
+    app.get('/promo*', function (req, res) {
+        res.send('Respuesta patrón promo* ');
+    })
+
+    app.get('/canciones/:genero/:id', function (req, res) {
+        var respuesta = 'id: ' + req.params.id + '<br>'
+            + 'Genero: ' + req.params.genero;
+        res.send(respuesta);
+    });
+
+    app.get('/canciones/:id', function (req, res) {
+        var respuesta = 'id: ' + req.params.id;
+        res.send(respuesta);
+    });
+
+    app.get("/canciones", function (req, res) {
         var respuesta = "";
         if (req.query.nombre != null)
             respuesta += 'Nombre: ' + req.query.nombre + '<br>';
@@ -8,20 +30,8 @@ module.exports = function(app) {
         res.send(respuesta);
     });
 
-    app.get('/canciones/:id', function(req, res) {
-        var respuesta = 'id: ' + req.params.id;
-        res.send(respuesta);
-    });
-
-    app.get('/canciones/:genero/:id', function(req, res) {
-        var respuesta = 'id: ' + req.params.id + '<br>'
-            + 'Genero: ' + req.params.genero;
-        res.send(respuesta);
-    });
-
-    app.get('/suma', function(req, res) {
+    app.get('/suma', function (req, res) {
         var respuesta = parseInt(req.query.num1) + parseInt(req.query.num2);
         res.send(String(respuesta));
     });
-
 };
