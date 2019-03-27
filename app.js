a4e50fcadd1d062759c72057085a48aa9026cbd9
Daniel <daniferna@outlook.com>
2019-03-28 00:36:44 +0100

    SDI-NodeJS-8.2 - Acceso a datos de Mongo desde Node.js

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
+// CADENA URL BBDD MONGODB
+// mongodb://admin:EIISDI2018$@tiendamusica-shard-00-00-0jvpg.mongodb.net:27017,tiendamusica-shard-00-01-0jvpg.mongodb.net:27017,tiendamusica-shard-00-02-0jvpg.mongodb.net:27017/test?ssl=true&replicaSet=tiendamusica-shard-0&authSource=admin&retryWrites=true
+
+
+
+
 // Módulos
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var swig = require('swig');
+var mongo = require('mongodb');
+
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -10,10 +18,12 @@ app.use(express.static('public'));
 
 // Variables
 app.set('port', 8081);
+app.set('db','mongodb://admin:EIISDI2018$@tiendamusica-shard-00-00-0jvpg.mongodb.net:27017,tiendamusica-shard-00-01-0jvpg.mongodb.net:27017,tiendamusica-shard-00-02-0jvpg.mongodb.net:27017/test?ssl=true&replicaSet=tiendamusica-shard-0&authSource=admin&retryWrites=true');
+
 
 //Rutas controladores por lógica
 require("./routes/rusuarios.js")(app, swig); // (app, param1, param2, etc.)
-require("./routes/rcanciones.js")(app, swig); // (app, param1, param2, etc.)
+require("./routes/rcanciones.js")(app, swig, mongo); // (app, param1, param2, etc.)
 
 
 // lanzar el servidor
