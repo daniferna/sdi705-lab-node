ea591e175e760b1faa6240066db365d0dbe136ad
Daniel <daniferna@outlook.com>
2019-04-11 01:42:47 +0200

    SDI-NodeJS-10.5- SW Consumir SW desde NodeJS

--- app.js
+++ app.js
@@ -14,6 +14,8 @@ app.use(function(req, res, next) {
     next();
 });
 
+var rest = require('request');
+app.set('rest',rest);
 var bodyParser = require('body-parser');
 var crypto = require('crypto');
 var jwt = require('jsonwebtoken');
